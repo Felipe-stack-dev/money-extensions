b78c856c0e3f9a5bc281d96e16babd97c63f119f
Felipe-stack-dev <felipetkg7@gmail.com>
2026-01-23 20:51:42 -0300

    Iniciando a publicação

--- MoneyExtension.Tests/MoneyExtensionTests.cs
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -1,10 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace MoneyExtension.Tests;
 
 [TestClass]
-public class UnitTest1
+public class MoneyExtensionTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void ShouldConvertDecimalToInt()
     {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
     }
 }
